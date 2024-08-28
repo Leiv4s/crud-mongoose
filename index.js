71585d564f8316fd0c49cfd3fe75d0ecf109593b
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-08-28 13:34:49 -0300

    Adicionando schema

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const {Schema} = mongoose;
 
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
   console.log('Conectado com o MongoDB');
+
+  const ocorrenciaSchema = new Schema({
+    _id: {
+      type: 'UUID',
+      default: () => randomUUID()
+    },
+    titulo: String,
+    descricao: String,
+    tipo: {
+      type: String,
+      enum: ['Assalto', 'Sequestro', 'Homicídio', 'Outros']
+    },
+    data: {
+      type: Date,
+      default: new Date()
+    },
+    localizacao: {
+      type: {
+        type: String, 
+        enum: ['Point'],
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    }
+  });
+
+  const Ocorrencia = mongoose.model('ocorrencias', ocorrenciaSchema);
+  
+  console.log(Ocorrencia);
+
 }
